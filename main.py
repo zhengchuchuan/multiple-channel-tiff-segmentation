ae9e515dd580df1c5460869b0e13959b6be3a205
chuchuaner <1531379706@qq.com>
2023-07-21 16:12:22 +0800

    对读取tif和写入tif抽象出一个函数,未做其他功能添加

--- main.py
+++ main.py
@@ -11,37 +11,65 @@ from PIL import Image
 子图命名规范:原图名称_000001开始递增
 '''
 
+'''
+读取tif文件,并将其转换为ndarray格式
+返回:数组数据,通道数,高度,宽度
+'''
 
-def tif_segmentation(read_file_root_path, read_file_name, tif_write_path='./output/segmented_tif/',
-                     xml_write_path='./output/segmented_xml/',
-                     mark_png_write_path='./output/markedPNG/', split_width=640, split_height=640):
-    # 打开TIF文件
+
+def read_tif(path):
     try:
-        dataset = gdal.Open(read_file_root_path + read_file_name, gdal.GA_Update)
+        dataset = gdal.Open(path, gdal.GA_Update)
         if dataset is None:
             raise IOError("无法打开TIF文件，请检查文件路径。")
     except IOError as e:  # IO异常
         print(str(e))
     except Exception as e:  # 其他异常
         print("其他异常：", str(e))
-
     # 获取TIF文件的通道数
-    num_channels = dataset.RasterCount
+    channels = dataset.RasterCount
     # 获取TIF文件的宽度和高度
     width = dataset.RasterXSize
     height = dataset.RasterYSize
+    # 将tif文件读取为ndarray数组
+    # 数据的格式(通道,高度,宽度)
+    arr_dataset = dataset.ReadAsArray()  # 转为numpy格式
+    del dataset
+    return arr_dataset, channels, height, width
+
+
+'''
+写入tif文件,
+参数:导出子图的路径,子图的数组,保存文件的类型,通道数,拆分的图像宽度,拆分的图像高度
+'''
+
+
+def write_tif(path, dataset, data_type, channels, width, height):
+    driver = gdal.GetDriverByName("GTiff")
+    # 创建tif文件
+    output_dataset = driver.Create(path, width, height, channels,
+                                   gdal_array.GDALTypeCodeToNumericTypeCode(data_type))
+    # 将子图数据写入TIF文件
+    '''
+    注意写入顺序,不同的图像可能需要更改,此次处理的图像尺寸格式为C,H,W,顺序错误图像会无法正确显示
+    问题:似乎切割出来的图像色相有一定的偏差
+    代办:自动识别图像的尺寸写入格式
+    '''
+    for channel in range(channels):
+        output_dataset.GetRasterBand(channel + 1).WriteArray(dataset[channel, :, :])
+    # 关闭TIF文件
+    output_dataset = None
+
+
+def tif_segmentation(read_file_root_path, read_file_name, tif_write_path='./output/segmented_tif/',
+                     xml_write_path='./output/segmented_xml/',
+                     mark_png_write_path='./output/markedPNG/', split_width=640, split_height=640):
+    # 读取TIF文件
+    arr_dataset, channels, height, width = read_tif(read_file_root_path + read_file_name)
     # 获取行列切片的数量
     row = math.ceil(height / split_height)
     col = math.ceil(width / split_width)
 
-    # 将tif文件读取为ndarray数组
-    # 数据的格式(通道,高度,宽度)
-    arr_dataset = dataset.ReadAsArray()
-
-    '''
-    保存tif测试
-    导出图像尺寸正常,但全黑,查看发现值全为0
-    '''
     # 保存切割后的子图并显示进度
     total_images = row * col
     with tqdm(total=total_images) as pbar:  # 使用tqdm的ProgressBar创建进度条对象并设置总进度
@@ -61,34 +89,18 @@ def tif_segmentation(read_file_root_path, read_file_name, tif_write_path='./outp
                 将切割后不能满足指定长宽的子图补全,且应该只在图像的右边或者下面补上白边
                 '''
                 # 创建子图,CHW!!
-                sub_image = np.zeros((num_channels, split_height, split_width), dtype=arr_dataset.dtype)  # 初始化为纯黑色的子图背景
-                sub_image[:num_channels, :sub_height, :sub_width] = arr_dataset[:, start_y:start_y+sub_height, start_x:start_x + sub_width]  # 求交集
-                # sub_image[:num_channels, :sub_height, :sub_width] = arr_dataset[:, 3000:3000+sub_height, 3000:3000 + sub_width] #测试用例
+                sub_image = np.zeros((channels, split_height, split_width), dtype=arr_dataset.dtype)  # 初始化为纯黑色的子图背景
+                sub_image[:channels, :sub_height, :sub_width] = arr_dataset[:, start_y:start_y + sub_height, start_x:start_x + sub_width]  # 求交集
+                # sub_image[:channels, :sub_height, :sub_width] = arr_dataset[:, 3000:3000+sub_height, 3000:3000 + sub_width] #测试用例
 
-                # 生成文件名,从000001开始
+                # 生成子图文件路径,从000001开始
                 file_number = r * col + c + 1
                 sub_file_name = read_file_name[:-4] + f"_{file_number:06d}.tif"
-
-                # 创建子图的输出文件
                 output_file = tif_write_path + sub_file_name
-                driver = gdal.GetDriverByName("GTiff")
-                output_dataset = driver.Create(output_file, split_width, split_height, num_channels,
-                                               gdal_array.GDALTypeCodeToNumericTypeCode(arr_dataset.dtype))
-
-                # 将子图数据写入TIF文件
-                '''
-                注意写入顺序,不同的图像可能需要更改,此次处理的图像尺寸格式为C,H,W,顺序错误图像会无法正确显示
-                问题:似乎切割出来的图像色相有一定的偏差
-                代办:自动识别图像的尺寸写入格式
-                '''
-                for channel in range(num_channels):
-                    output_dataset.GetRasterBand(channel + 1).WriteArray(sub_image[channel, :, :])
-                # 关闭TIF文件
-                output_dataset = None
+                # 写入tif文件
+                write_tif(output_file, sub_image, arr_dataset.dtype, channels, split_width, split_height)
                 # 更新进度条，表示处理了一个图像
                 pbar.update(1)
-    # 关闭原始TIF文件
-    dataset = None
 
 
 if __name__ == '__main__':
